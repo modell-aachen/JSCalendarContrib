84f3f5823f441eb83d620a8185a8983107686cc1
Maik Glatki <glatki@modell-aachen.de>
2015-09-21 13:15:15 +0200

    Use new MaintenancePlugin API.
    Ticket: None
    Release-Note(change,perldev){
    Use new MaintenancePlugin API.
    }

--- lib/Foswiki/Contrib/JSCalendarContrib.pm
+++ lib/Foswiki/Contrib/JSCalendarContrib.pm
@@ -230,6 +230,23 @@ HERE
         'JSCalendarContrib/calendar' );
 }
 
+# MaintenancePlugin integration
+sub maintenanceHandler {
+    Foswiki::Plugins::MaintenancePlugin::registerCheck("jscalendercontrib:settings_language", {
+        name => "JSCalenderContrib language setting",
+        description => "JSCALENDARCONTRIB_LANG setting in missing in SitePreferences",
+        check => sub {
+            my $result = { result => 0 };
+            my ( $spmeta, $sp ) = Foswiki::Func::readTopic( 'Main', 'SitePreferences');
+            if ( $spmeta->getPreference( "JSCALENDARCONTRIB_LANG" ) eq '' ) {
+                $result->{result} = 1;
+                $result->{priority} = 1;
+                $result->{solution} = "Add '   * Set JSCALENDARCONTRIB_LANG = %<nop>LANGUAGE%' to [[Main.SitePreferences]].";
+            }
+            return $result;
+        }
+    });
+}
 1;
 __END__
 Foswiki - The Free and Open Source Wiki, http://foswiki.org/
